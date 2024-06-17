8436fd9bc8bf4e3b74cd69575d3d77bdfaddfdef
unitx <paresh@unitxlabs.com>
2024-06-17 16:39:17 -0700

    chore: Exercise 3

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,19 +2,34 @@
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
         
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.stack = Node(None)
   
     def push(self, new_data): 
-        
+        if self.stack.data == None:
+            self.stack.data = new_data
+        else:
+            new_node = Node(new_data)
+            new_node.next = self.stack
+            self.stack = new_node
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        slow = self.stack
+        fast = self.stack
+
+        if self.stack != None:
+            while fast != None and fast.next != None:
+                slow = slow.next
+                fast = fast.next.next
+            print("The middle element is: ", slow.data) 
 
 # Driver code 
 list1 = LinkedList() 
